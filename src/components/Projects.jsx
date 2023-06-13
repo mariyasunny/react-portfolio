2182cda9bfd271a6d717fd30de6956b10a759995
Mariya Sunny <mariyasunny09@gmail.com>
2023-06-13 12:09:18 -0400

    Saving changes to github

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,6 +7,8 @@ import js from '../assets/images/javascript.png';
 import linux from '../assets/images/linux.png';
 import asp from '../assets/images/asp.png';
 import bootstrap from '../assets/images/bootstrap.png';
+import php from '../assets/images/php.png';
+import mysql from '../assets/images/mysql.png';
 import img1 from '../assets/images/kidsHome.png'
 import img2 from '../assets/images/kidsShop.png'
 import img3 from '../assets/images/kidsSeller.png'
@@ -106,13 +108,12 @@ heroVid: {
 
 
       
+      
       <div className='divContainer'>
       <img src={asp} alt='asp' width={40}></img> 
       <img src={bootstrap} alt='bootstrap' width={40}></img> 
         <p style={styles.aboutText}>I have created a simple portfolio site using ASP.Net and bootstrap and published in <a href='https://msvportfolio.azurewebsites.net/Home/Projects' style={{color:'#00FFFF'}}>Microsoft Azure.com</a> </p>
-            
-       
-      
+         
         <iframe width="560" height="315" src="https://www.youtube.com/embed/sbuodh-K3RQ" title="YouTube video player" frameBorder="0" allow="autoplay; loop; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
       
       </div>
@@ -130,8 +131,8 @@ heroVid: {
       </div>
 
       <div>   
-        <img src={html} alt='java' width={40}></img>
-        <img src={css} alt='java' width={40}></img> 
+        <img src={html} alt='html' width={40}></img>
+        <img src={css} alt='css' width={40}></img> 
         <img src={js} alt='java' width={40}></img>
 
         <p style={styles.aboutText}>I have designed and created a website for an online burgershop using HTML5 and CSS Grid. 
@@ -141,6 +142,20 @@ heroVid: {
 
         <iframe width="560" height="315" src="https://www.youtube.com/embed/nUcrMbYyOWk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
      </div>  
+
+     <div className='divContainer'>
+      <img src={php} alt='php' width={40}></img> 
+      <img src={html} alt='html' width={40}></img>
+      <img src={css} alt='css' width={40}></img> 
+      <img src={mysql} alt='mysql' width={40}></img>
+
+
+      
+        <p style={styles.aboutText}>I have designed created a website for an Online Bakery shop using PHP,HTML,CSS,javaScript and MySQL. View <a href='https://heavenlytreats2023.000webhostapp.com/loginForm.php'target='_blank' rel="noreferrer" style={{color:'#00FFFF'}}>source code</a> </p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/SYLe8O5qnRI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      
+      </div> <br/> <br/>
+
 
      <div className='divContainer'>          
               <h3 size={20} style={{color:'#fff'}}>UI and Software Applications</h3>
